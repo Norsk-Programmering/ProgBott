72daa2f6f22de72f1040d0774b95762a529dc0ff
Roxedus <me@roxedus.dev>
2020-07-14 00:07:04 +0200

    Added error-handling to webserver

--- cogs/utils/server.py
+++ cogs/utils/server.py
@@ -21,6 +21,16 @@ def index():
     return "Hello, world!"
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', error=e, code=404), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error.html', error=e, code=500), 500
+
+
 @app.route("/github/oauth/callback")
 def callback():
     query_code = request.args.get("code")
