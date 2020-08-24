ff2322cac2799f16c904aa48a80f477723d43d6d
Roxedus <me@roxedus.dev>
2020-08-24 14:50:17 +0200

    less filter

--- cogs/broder.py
+++ cogs/broder.py
@@ -8,20 +8,19 @@ class Broder(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.author.bot and message.mentions:
+        if not message.author.bot:
             await self._filter(message)
 
     @commands.Cog.listener()
     async def on_message_edit(self, before, after):
-        if not after.author.bot and after.mentions \
-                and (after.edited_at.timestamp() - before.created_at.timestamp()) < 60:
+        if not after.author.bot and (after.edited_at.timestamp() - before.created_at.timestamp()) < 60:
             await self._filter(after)
 
     async def _filter(self, message, **kwarg):
         word_ = "bruh"
         content_ = message.content.lower()
         if word_ in content_:
-            await message.channel.send.send("bruh")
+            await message.channel.send("bruh")
 
 
 def setup(bot):
