a054f81151ddf46a008c53b4de4a3d2422c35a08
Roxedus <me@roxedus.dev>
2020-08-24 14:42:51 +0200

    broder

---
+++ cogs/broder.py
@@ -0,0 +1,28 @@
+# Discord Packages
+from discord.ext import commands
+
+
+class Broder(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.author.bot and message.mentions:
+            await self._filter(message)
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if not after.author.bot and after.mentions \
+                and (after.edited_at.timestamp() - before.created_at.timestamp()) < 60:
+            await self._filter(after)
+
+    async def _filter(self, message, **kwarg):
+        word_ = "bruh"
+        content_ = message.content.lower()
+        if word_ in content_:
+            await message.channel.send.send("bruh")
+
+
+def setup(bot):
+    bot.add_cog(Broder(bot))
